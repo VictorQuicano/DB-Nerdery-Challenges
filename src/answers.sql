50278d14e3894a3e08c6a2e2b95ed0cbe4ee68d0
Alejandro Quicaño <alejandroxd25k@gmail.com>
2026-01-20 08:49:47 -0500

    imp: SQL queries

--- src/answers.sql
+++ src/answers.sql
@@ -1,24 +1,33 @@
 -- Your answers here:
 -- 1
-SELECT c.name, count(*) FROM countries c INNER JOIN states s ON c.id = s.country_id GROUP BY c.name ;
+SELECT 
+    c.name, 
+    count(*) 
+FROM countries c 
+INNER JOIN states s 
+ON c.id = s.country_id 
+GROUP BY c.name ;
 
 -- 2
-SELECT COUNT(*) employees_without_bosses FROM employees WHERE supervisor_id IS NULL;
+SELECT 
+    COUNT(*) employees_without_bosses 
+FROM employees 
+WHERE supervisor_id IS NULL;
 
 -- 3
-SELECT c.name name, n.address, n.count 
-    FROM countries c 
-    INNER JOIN (
-        SELECT o.country_id, o.address, count(*) count 
-        FROM offices o 
-        INNER JOIN employees e 
-        ON  o.id = e.office_id 
-        GROUP BY o.id
-    ) n 
-    ON c.id = n.country_id 
-    ORDER BY 
-    count DESC
-    LIMIT 5 ;
+SELECT
+    c.name AS country_name,
+    o.address,
+    COUNT(e.id) AS employee_count
+FROM countries c
+JOIN offices o
+    ON c.id = o.country_id
+JOIN employees e
+    ON o.id = e.office_id
+GROUP BY c.id, c.name, o.id, o.address
+ORDER BY employee_count DESC
+LIMIT 5;
+
 
 -- 4
 SELECT e.id supervisor_id, COUNT(*) 
@@ -63,21 +72,21 @@ LIMIT 1
 );
 
 -- 8
-SELECT e.uuid, e.full_name, e.email, e.job_title, o.company, o.country, o.state, e.boss_name 
-FROM (
-    SELECT e.uuid, e.first_name || ' ' || e.last_name full_name, e.email, e.job_title, s.first_name boss_name, e.office_id 
-    FROM employees e 
-    INNER JOIN employees s 
-    ON e.supervisor_id = s.id
-) e
-LEFT JOIN (
-    SELECT o.id, o.name company, s.country country, s.state state 
-    FROM offices o 
-    INNER JOIN (
-        SELECT s.id state_id, s.name state, c.name country 
-        FROM states s 
-        INNER JOIN countries c 
-        ON s.country_id = c.id
-    ) s 
-    ON o.state_id = s.state_id) o
-ON e.office_id = o.id;
+SELECT 
+    e.uuid,
+    e.first_name || ' ' || e.last_name AS full_name,
+    e.email,
+    e.job_title,
+    o.name AS company,
+    c.name AS country,
+    s.name AS state,
+    b.first_name AS boss_name
+FROM employees e
+INNER JOIN employees b
+    ON e.supervisor_id = b.id
+LEFT JOIN offices o
+    ON e.office_id = o.id
+LEFT JOIN states s
+    ON o.state_id = s.id
+LEFT JOIN countries c
+    ON s.country_id = c.id;
